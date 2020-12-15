9c0b2de58fcf971689243fd392722cd79dc6485a
Stephanie Flora <twinsf2@gmail.com>
2020-12-14 18:05:14 -0800

    End of weekend work

--- js/script.js
+++ js/script.js
@@ -1,36 +1,113 @@
 document.getElementById("question").focus();
 document.getElementById("question").select();
 
+// GRPAHING THE DATA -------------------------------------------------------------------------------------------
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(231,233,237)'
+  };
+  
+var ctx = document.getElementById('myChart');
+// console.log(ctx)
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: null,
+    datasets: [{
+        label: 'Random integer',
+        data: null,
+    }]
+  },
+  options: {
+    responsive: true,
+    borderColor: window.chartColors.yellow,
+    borderWidth: 15,
+    defaultFontSize: 30,
+    defaultFontColor: "#fff",
+    borderColor: "rgb(255, 255, 255)",
+    defaultColor: "rgba(255,255,255,1)",
+    fill: false,
+    title: {
+        display: true,
+        text: 'Random Number Generated for Answer'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: true
+      },   
+      scales: {
+        xAxes: [{
+          display: true,
+          ticks: {
+            min: 0
+          }
+        }],
+        yAxes: [{
+          display: true
+        }],
+      }       
+  }
+});
 
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ASK  BUTTON SECTION
+// ------------------------------------------------------------------------------------------------------------
 function ask(event) {
     event.preventDefault();
     const formobj  = document.getElementById('theform');
     // console.log(formobj)
     
     formobj.classList.add('hidden');  //Addes the hidden class to the form so it vanishes
-    answer = randanswer ();
-
-    // for (let i = 0; i < 100; i++) {
-    //     answer = randanswer ();
-    // };  
+    let {answer,rnum} = randanswer ();
 
     const questdiv  = document.getElementById('questdiv');
     questdiv.classList.remove('hidden');
     const p = document.createElement('p');
     questdiv.appendChild(p);
     p.innerHTML = formobj[0].value;
-    p.classList.add('pformat');
+    p.classList.add('glow');
 
     const ansdiv  = document.getElementById('ansdiv');
-    const p1 = document.createElement('p');
-    ansdiv.appendChild(p1);
-    p1.innerHTML = answer;
-    p1.classList.add('pformat2');
+    const m8txt   = document.getElementById('centered');
+    m8txt.innerHTML = answer;
+    console.log(answer)
+    // m8txt.classList.add('pformat2');
+    m8txt.classList.add('glow2');
+    m8txt.style.fontSize = '50px';
 
     const formobj2  = document.getElementById('theform2');
     formobj2.classList.remove('hidden');
+
+    let Glabel = sessionStorage.getItem('chartlabels');
+    let Gdata  = sessionStorage.getItem('chartdata');
+    if (Gdata) {
+        myLineChart.data.labels = JSON.parse(Glabel);
+        myLineChart.data.datasets[0].data = JSON.parse(Gdata);
+    }
+
+    lstr = myLineChart.data.labels;
+
+    addData(myLineChart, lstr.length+1, rnum);
+    console.log(myLineChart);
+
+    sessionStorage.setItem('chartlabels',JSON.stringify(myLineChart.data.labels));
+    sessionStorage.setItem('chartdata',JSON.stringify(myLineChart.data.datasets[0].data));
 }  
 
+// RESET BUTTON ---------------------------------------------------------------
 function reset(event) {
     event.preventDefault();
     const formobj  = document.getElementById('theform');
@@ -41,9 +118,10 @@ function reset(event) {
     questdiv.classList.add('hidden');
 }
 
+//  Function that returns randon number and answer to ask ()
 function randanswer () {
     let rnum = Math.floor(Math.random() * 20);
-    console.log(rnum);
+    // console.log(rnum);
     let answer = 0; 
     switch(rnum) {
         case 0:
@@ -110,6 +188,8 @@ function randanswer () {
             answer = "Sorry you are on your own";
             break
     }
-
-    return answer;
+ 
+    return {answer, rnum};
 }
+
+
